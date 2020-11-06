b830159717d8610238221bb89334831ad8471fdf
Samuel Trahan <samtrahan91@gmail.com>
2020-11-06 13:57:27 -0600

    section and footer components added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
+import Section from './components/Section/Section';
+import Footer from './components/Footer/Footer';
 
 export default function App() {
   return (
     <div>
       <NavBar />
       <Banner />
+      <Section />
+      <Footer />
     </div>
   )
 }
